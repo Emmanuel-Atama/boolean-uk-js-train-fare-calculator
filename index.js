9d28dc1a9dd9dd50d31501a59e899e2d9fd7f706
Emmanuel Atama <emmanuelatama@outlook.com>
2021-07-14 10:36:25 +0100

    Recent update

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ const fareToEuston = distanceToEustonInKm * pricePerKm;
 console.log(fareToEuston);
 
 if (passengerAge < 18) alert("Apply 20% Doscount");
-else alert("Too Old for Junior Passenger Discount");
+else alert("Not eligible for Junior Passenger Discount");
 if (passengerAge > 65) alert("Apply 40% Discount");
-else alert("Not Old Enough for Senior Passenger Discount");
+else alert("Not eligible for Senior Passenger Discount");
 
 const juniorPassengersDiscount = 0.2;
 const seniorPassengersDiscount = 0.4;
 
-const fareForUnder18ToPaddington =
+const fareForUnder18ToPaddington =                       
   fareToPaddington - fareToPaddington * juniorPassengersDiscount;
 console.log(fareForUnder18ToPaddington);
 
